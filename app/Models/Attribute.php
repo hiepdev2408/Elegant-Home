4fa88e7b9c2fc209ed3402e197901b9319bc1b4f
duyhiep <dinhduyhiep2408@gmail.com>
2024-10-01 13:30:37 +0700

    migrations-database

---
+++ app/Models/Attribute.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'variant_id',
+        'name',
+    ];
+
+    public function variant(){
+        return $this->belongsTo(Variant::class);
+    }
+
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
+}
