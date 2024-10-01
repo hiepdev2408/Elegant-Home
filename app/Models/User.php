5e164cd9fb4b7a3a7c15908a37dc25197284ceb0
duyhiep <dinhduyhiep2408@gmail.com>
2024-10-02 06:46:06 +0700

    realtionships-user

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function province(){
+        return $this->belongsTo(Province::class, 'province_id', 'code');
+    }
+
+    public function district(){
+        return $this->belongsTo(District::class, 'district_id', 'code');
+    }
+
+    public function ward(){
+        return $this->belongsTo(Ward::class, 'ward_id', 'code');
+    }
 }
