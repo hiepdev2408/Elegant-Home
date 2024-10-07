5968b2c5339c9c4b3e46164bd0349d70327fffe0
Lê Văn Đông <donglvph35428@fpt.edu.vn>
2024-10-07 22:01:38 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -10,7 +10,8 @@ Route::prefix('admin')
         Route::get('/', function () {
             return view(view: 'admin.dashboard');
         });
-      Route::resource('users', UserController::class);
+
+        Route::resource('users', UserController::class);
 
         Route::prefix('categories')
             ->name('categories.')
@@ -38,8 +39,31 @@ Route::prefix('admin')
                 Route::delete('/delete/{category}', [CategoryController::class, 'destroy'])
                     ->name('destroy');
             });
+        Route::get('index', function () {
+            return view('admin.products.index');
+        })->name('product.index');
     });
 Route::get('/', function () {
     return view('client.home');
 });
 
+Route::group(['prefix' => 'account'], function () {
+    Route::get('/login', [AccountController::class, 'login'])->name('login');
+    Route::post('/login_check', [AccountController::class, 'check_login'])->name('login.submit');
+
+    Route::get('/register', [AccountController::class, 'register'])->name('register');
+    Route::post('/register_check', [AccountController::class, 'check_register'])->name('register.submit');
+
+    Route::get('/veryfy_account/{email}', [AccountController::class, 'veryfy'])->name('veryfy');
+
+    Route::get('/password/forgot', [AccountController::class, 'showForgotPasswordForm'])->name('password.request');
+Route::post('/password/email', [AccountController::class, 'sendResetLinkEmail'])->name('password.email');
+
+
+Route::get('/password/reset/{token}', [AccountController::class, 'showResetForm'])->name('password.reset');
+Route::post('/password/reset', [AccountController::class, 'reset'])->name('password.update');
+});
+
+Route::get('admin', function () {
+    return view('admin.dashboard');
+});
