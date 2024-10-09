2ee364f1749eb955cb398323d85c1ab8c7ff63a2
duyhiep <dinhduyhiep2408@gmail.com>
2024-10-09 22:57:15 +0700

    update_web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 use App\Http\Controllers\AccountController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ContactFormController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
